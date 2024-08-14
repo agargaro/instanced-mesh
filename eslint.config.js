f9cac2e47d4bfe14fbf82369681ffe83bd94f5de
ANDREA GARGARO <devgargaro@gmail.com>
2024-08-14 16:49:35 +0200

    no explicit any rule

--- eslint.config.js
+++ eslint.config.js
@@ -4,8 +4,13 @@ import tseslint from "typescript-eslint";
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    "rules": {
+      "@typescript-eslint/no-explicit-any": "info"
+    }
+  }
 ];
