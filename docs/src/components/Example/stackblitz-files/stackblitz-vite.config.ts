1764d29737a254f52685fad96d0cc8ced649dde1
ANDREA GARGARO <devgargaro@gmail.com>
2024-12-13 20:06:31 +0100

    fix: stackb

--- docs/src/components/Example/stackblitz-files/stackblitz-vite.config.ts
+++ docs/src/components/Example/stackblitz-files/stackblitz-vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [],
   resolve: {
     alias: {
       $lib: path.resolve('./src/lib')
