9a5bc8fe35d6deb7039bd883fa686674847f46ab
ANDREA GARGARO <devgargaro@gmail.com>
2024-12-30 21:04:08 +0100

    update readme

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,21 @@ import { type InstancedMesh2 } from './core/InstancedMesh2.js';
 export * from './core/InstancedEntity.js';
 export * from './core/InstancedMesh2.js';
 export * from './core/InstancedMeshBVH.js';
+
 export * from './core/feature/Capacity.js';
 export * from './core/feature/FrustumCulling.js';
 export * from './core/feature/Instances.js';
 export * from './core/feature/LOD.js';
+export * from './core/feature/Morph.js';
 export * from './core/feature/Raycasting.js';
 export * from './core/feature/Uniforms.js';
+
 export * from './core/utils/GLInstancedBufferAttribute.js';
 export * from './core/utils/InstancedRenderList.js';
+export * from './core/utils/SquareDataTexture.js';
 
 export * from './shaders/ShaderChunk.js';
+
 export * from './shaders/chunks/instanced_pars_vertex.glsl.js';
 export * from './shaders/chunks/instanced_color_pars_vertex.glsl.js';
 export * from './shaders/chunks/instanced_vertex.glsl.js';
