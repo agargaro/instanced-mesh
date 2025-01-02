82c1f2ed9926eb7be56262acf9cbe33083ba66c6
ANDREA GARGARO <devgargaro@gmail.com>
2025-01-02 01:24:55 +0100

    wip

--- examples/skeleton.ts
+++ examples/skeleton.ts
@@ -3,7 +3,7 @@ import { AnimationMixer, DirectionalLight, HemisphereLight, Scene, SkinnedMesh }
 import { GLTF, GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { InstancedMesh2 } from '../src/index.js';
 
-const count = 3;
+const count = 7;
 
 const camera = new PerspectiveCameraAuto().translateZ(-5).rotateY(Math.PI);
 const scene = new Scene();
@@ -15,55 +15,46 @@ const light = new DirectionalLight(0xffffff, 1);
 light.position.set(200, 1000, 50);
 
 const glb = await Asset.load<GLTF>(GLTFLoader, 'https://threejs.org/examples/models/gltf/Soldier.glb');
+
 const dummy = glb.scene.children[0].children[0] as SkinnedMesh;
-
-dummy.visible = false;
 glb.scene.children[0].children[1].visible = false;
+dummy.visible = false;
 
-const soldiers = new InstancedMesh2(dummy.geometry, dummy.material, { capacity: count });
+dummy.skeleton.bones.forEach((bone) => {
+  bone.matrixAutoUpdate = false;
+  bone.matrixWorldAutoUpdate = false;
+});
+
+const soldiers = new InstancedMesh2<{ mixer: AnimationMixer }>(dummy.geometry, dummy.material, { capacity: count, createInstances: true });
 
 soldiers.addInstances(count, (obj, index) => {
   obj.position.copy(dummy.position);
-  obj.position.x += index * 100 - 100;
+  obj.position.x += index * 100 - 300;
   obj.quaternion.copy(dummy.quaternion);
   obj.scale.copy(dummy.scale);
+
+  const mixer = new AnimationMixer(glb.scene);
+  mixer.clipAction(glb.animations[index % 2]).play();
+  obj.mixer = mixer;
 });
 
-const mixer = new AnimationMixer(glb.scene);
-const action = mixer.clipAction(glb.animations[0]);
-action.play();
-
-// const mixer2 = new AnimationMixer(glb.scene);
-// const action2 = mixer2.clipAction(glb.animations[1]);
-// action2.play();
-
-// const mixer3 = new AnimationMixer(glb.scene);
-// const action3 = mixer3.clipAction(glb.animations[3]);
-// action3.play();
-
-mixer.update(0);
-soldiers.setSkeletonAt(0, dummy.skeleton);
-soldiers.setSkeletonAt(1, dummy.skeleton);
-soldiers.setSkeletonAt(2, dummy.skeleton);
-// mixer2.update(0);
-// soldiers.setSkeletonAt(1, dummy.skeleton);
-// mixer3.update(0);
-// soldiers.setSkeletonAt(2, dummy.skeleton);
-
-const offset = [1, 1, 1];
+for (let i = 0; i < count; i++) {
+  soldiers.setSkeletonAt(i, dummy.skeleton);
+}
 
 scene.on('animate', (e) => {
-  debugger
-  mixer.setTime(e.total * offset[0]);
-  soldiers.setSkeletonAt(0, dummy.skeleton);
-  mixer.setTime(e.total * offset[1]);
-  soldiers.setSkeletonAt(1, dummy.skeleton);
-  mixer.setTime(e.total * offset[2]);
-  soldiers.setSkeletonAt(2, dummy.skeleton);
-  // mixer2.setTime(e.total);
-  // soldiers.setSkeletonAt(1, dummy.skeleton);
-  // mixer3.setTime(e.total);
-  // soldiers.setSkeletonAt(2, dummy.skeleton);
+  const instances = soldiers.instances;
+
+  for (let i = 0; i < count; i++) {
+    instances[i].mixer.setTime(e.total);
+
+    dummy.skeleton.bones.forEach((bone) => { // auto?
+      bone.updateMatrix();
+      bone.matrixWorld.multiplyMatrices(bone.parent.matrixWorld, bone.matrix);
+    });
+
+    soldiers.setSkeletonAt(i, dummy.skeleton);
+  }
 });
 
 glb.scene.children[0].add(soldiers);
--- src/core/feature/Skeleton.ts
+++ src/core/feature/Skeleton.ts
@@ -1,24 +1,26 @@
-import { Matrix4, Skeleton, Vector3, Vector4 } from 'three';
+import { Matrix4, Skeleton } from 'three';
 import { InstancedMesh2 } from '../InstancedMesh2.js';
 import { SquareDataTexture } from '../utils/SquareDataTexture.js';
 
 declare module '../InstancedMesh2.js' {
   interface InstancedMesh2 {
+    /**
+     *
+     * @param id
+     * @param skeleton
+     */
     getSkeletonAt(id: number, skeleton?: Skeleton): Skeleton;
+    /**
+     *
+     * @param id
+     * @param skeleton
+     */
     setSkeletonAt(id: number, skeleton: Skeleton): void;
-    bind(skeleton: Skeleton, bindMatrix: Matrix4): void;
-    normalizeSkinWeights(): void;
-    applyBoneTransform(index: number, vector: Vector3): Vector3;
   }
 }
 
 const _offsetMatrix = new Matrix4();
 const _identityMatrix = new Matrix4();
-const _basePosition = new Vector3();
-const _skinIndex = new Vector4();
-const _skinWeight = new Vector4();
-const _matrix4 = new Matrix4();
-const _vector3 = new Vector3();
 
 InstancedMesh2.prototype.getSkeletonAt = function (id: number, skeleton?: Skeleton) {
   throw new Error('\'getSkeletonAt\' not implemented yet.');
@@ -46,61 +48,4 @@ InstancedMesh2.prototype.setSkeletonAt = function (id: number, skeleton: Skeleto
   }
 
   this.boneTexture.enqueueUpdate(id);
-};
-
-InstancedMesh2.prototype.bind = function (skeleton: Skeleton, bindMatrix: Matrix4) {
-  this.skeleton = skeleton;
-
-  if (bindMatrix === undefined) {
-    this.updateMatrixWorld(true);
-    this.skeleton.calculateInverses();
-    bindMatrix = this.matrixWorld;
-  }
-
-  this.bindMatrix.copy(bindMatrix);
-  this.bindMatrixInverse.copy(bindMatrix).invert();
-};
-
-InstancedMesh2.prototype.normalizeSkinWeights = function () {
-  const vector = new Vector4();
-  const skinWeight = this.geometry.attributes.skinWeight;
-
-  for (let i = 0, l = skinWeight.count; i < l; i++) {
-    vector.fromBufferAttribute(skinWeight, i);
-    const scale = 1.0 / vector.manhattanLength();
-
-    if (scale !== Infinity) {
-      vector.multiplyScalar(scale);
-    } else {
-      vector.set(1, 0, 0, 0); // do something reasonable
-    }
-
-    skinWeight.setXYZW(i, vector.x, vector.y, vector.z, vector.w);
-  }
-};
-
-InstancedMesh2.prototype.applyBoneTransform = function (index: number, vector: Vector3) {
-  const skeleton = this.skeleton;
-  const geometry = this.geometry;
-
-  _skinIndex.fromBufferAttribute(geometry.attributes.skinIndex, index);
-  _skinWeight.fromBufferAttribute(geometry.attributes.skinWeight, index);
-
-  _basePosition.copy(vector).applyMatrix4(this.bindMatrix);
-
-  vector.set(0, 0, 0);
-
-  for (let i = 0; i < 4; i++) {
-    const weight = _skinWeight.getComponent(i);
-
-    if (weight !== 0) {
-      const boneIndex = _skinIndex.getComponent(i);
-
-      _matrix4.multiplyMatrices(skeleton.bones[boneIndex].matrixWorld, skeleton.boneInverses[boneIndex]);
-
-      vector.addScaledVector(_vector3.copy(_basePosition).applyMatrix4(_matrix4), weight);
-    }
-  }
-
-  return vector.applyMatrix4(this.bindMatrixInverse);
 };
