00db8fa31263371ed873a1a8dbcad07761b277a5
ANDREA GARGARO <devgargaro@gmail.com>
2025-01-04 01:03:15 +0100

    wip

--- examples/skeleton.ts
+++ examples/skeleton.ts
@@ -1,31 +1,23 @@
 import { Asset, Main, PerspectiveCameraAuto } from '@three.ez/main';
-import { AnimationMixer, DirectionalLight, HemisphereLight, Scene, SkinnedMesh } from 'three';
+import { AmbientLight, AnimationMixer, Mesh, Scene } from 'three';
 import { GLTF, GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { InstancedMesh2 } from '../src/index.js';
 
 const count = 7;
 const animationMaxFps = 30;
-let time = 0;
 
 const camera = new PerspectiveCameraAuto().translateZ(-5).rotateY(Math.PI);
 const scene = new Scene();
 const main = new Main();
 main.createView({ scene, camera, enabled: false, backgroundColor: 0x99DDFF });
 
-const hemi = new HemisphereLight(0x99DDFF, 0x669933, 1 / 3);
-const light = new DirectionalLight(0xffffff, 1);
-light.position.set(200, 1000, 50);
-
 const glb = await Asset.load<GLTF>(GLTFLoader, 'https://threejs.org/examples/models/gltf/Soldier.glb');
 
-const dummy = glb.scene.children[0].children[0] as SkinnedMesh;
+const dummy = glb.scene.children[0].children[0] as Mesh;
 glb.scene.children[0].children[1].visible = false;
 dummy.visible = false;
 
 const indexAnimation = [0, 1, 3];
-
-// const soldiers = new InstancedMesh2<{ mixer: AnimationMixer }>(dummy.geometry, dummy.material, { capacity: count, createInstances: true });
-// soldiers.initSkeleton(dummy.skeleton);
 
 const soldiers = InstancedMesh2.createFrom<{ mixer: AnimationMixer }>(dummy, { capacity: count, createInstances: true });
 
@@ -38,6 +30,11 @@ soldiers.addInstances(count, (obj, index) => {
   obj.mixer = new AnimationMixer(glb.scene);
   obj.mixer.clipAction(glb.animations[indexAnimation[index % 3]]).play();
 });
+
+glb.scene.children[0].add(soldiers);
+scene.add(new AmbientLight(), glb.scene);
+
+let time = 0;
 
 scene.on('animate', (e) => {
   time += e.delta;
@@ -52,6 +49,3 @@ scene.on('animate', (e) => {
 
   time = time % (1 / animationMaxFps);
 });
-
-glb.scene.children[0].add(soldiers);
-scene.add(light, hemi, glb.scene);
