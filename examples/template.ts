5040bcc62fb946d3ace77b544ef4a74ed33147c5
ANDREA GARGARO <devgargaro@gmail.com>
2025-01-12 16:31:13 +0100

    revert template example

--- examples/template.ts
+++ examples/template.ts
@@ -1,5 +1,5 @@
 import { Main, PerspectiveCameraAuto } from '@three.ez/main';
-import { BoxGeometry, Mesh, MeshBasicMaterial, MeshNormalMaterial, Scene } from 'three';
+import { BoxGeometry, MeshNormalMaterial, Scene } from 'three';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
 import { InstancedMesh2 } from '../src/index.js';
 
@@ -10,41 +10,9 @@ main.createView({ scene, camera });
 const controls = new OrbitControls(camera, main.renderer.domElement);
 controls.update();
 
-const material = new MeshNormalMaterial();
-const material2 = new MeshNormalMaterial();
-const material3 = new MeshNormalMaterial();
+const boxes = new InstancedMesh2(new BoxGeometry(), new MeshNormalMaterial());
+scene.add(boxes);
 
-const boxes = new InstancedMesh2(new BoxGeometry(), material);
-const boxes2 = new InstancedMesh2(new BoxGeometry(), material2);
-const boxes3 = new InstancedMesh2(new BoxGeometry(), material3);
-
-boxes.addInstances(1, (obj, index) => {
-  obj.position.x = index - 5;
-  obj.position.y = -1.5;
+boxes.addInstances(100, (obj, index) => {
+  obj.position.randomDirection().multiplyScalar(Math.random() * 5);
 });
-
-boxes2.addInstances(5, (obj, index) => {
-  obj.position.x = index - 5;
-  obj.position.y = 1.5;
-});
-
-boxes3.addInstances(5, (obj, index) => {
-  obj.position.x = index - 5;
-  obj.position.y = 0;
-});
-
-const box = new Mesh(new BoxGeometry(), material).translateX(2).translateY(1.5);
-const box2 = new Mesh(new BoxGeometry(), material2).translateX(2).translateY(0);
-const box3 = new Mesh(new BoxGeometry(), material3).translateX(2).translateY(-1.5);
-
-scene.add(box, box2, box3, boxes, boxes2, boxes3);
-
-const overrideMaterial = new MeshBasicMaterial();
-
-setInterval(() => {
-  scene.overrideMaterial = scene.overrideMaterial !== overrideMaterial ? overrideMaterial : null;
-  boxes.addInstances(1, (obj, index) => {
-    obj.position.x = index - 5;
-    obj.position.y = -1.5;
-  });
-}, 1000);
