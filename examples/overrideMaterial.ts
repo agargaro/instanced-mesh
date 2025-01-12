62111cdd9b3977140d9834f28170e16fbcabfe92
ANDREA GARGARO <devgargaro@gmail.com>
2025-01-12 16:20:31 +0100

    wip

---
+++ examples/overrideMaterial.ts
@@ -0,0 +1,50 @@
+import { Main, PerspectiveCameraAuto } from '@three.ez/main';
+import { AmbientLight, BoxGeometry, Mesh, MeshBasicMaterial, MeshLambertMaterial, MeshNormalMaterial, Scene } from 'three';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { InstancedMesh2 } from '../src/index.js';
+
+const camera = new PerspectiveCameraAuto().translateZ(10);
+const scene = new Scene();
+const main = new Main(); // init renderer and other stuff
+main.createView({ scene, camera });
+const controls = new OrbitControls(camera, main.renderer.domElement);
+controls.update();
+
+const material = new MeshLambertMaterial();
+const material2 = new MeshLambertMaterial();
+const material3 = new MeshLambertMaterial();
+
+const boxes = new InstancedMesh2(new BoxGeometry(), material, { capacity: 1 });
+const boxes2 = new InstancedMesh2(new BoxGeometry(), material2);
+const boxes3 = new InstancedMesh2(new BoxGeometry(), material3);
+
+boxes.addInstances(1, (obj, index) => {
+  obj.position.x = index - 5;
+  obj.position.y = -1.5;
+});
+
+boxes2.addInstances(5, (obj, index) => {
+  obj.position.x = index - 5;
+  obj.position.y = 1.5;
+});
+
+boxes3.addInstances(5, (obj, index) => {
+  obj.position.x = index - 5;
+  obj.position.y = 0;
+});
+
+const box = new Mesh(new BoxGeometry(), material).translateX(2).translateY(1.5);
+const box2 = new Mesh(new BoxGeometry(), material2).translateX(2).translateY(0);
+const box3 = new Mesh(new BoxGeometry(), material3).translateX(2).translateY(-1.5);
+
+scene.add(box, boxes, box2, boxes2, boxes3, box3, new AmbientLight());
+
+const overrideMaterial = new MeshBasicMaterial();
+
+setInterval(() => {
+  scene.overrideMaterial = scene.overrideMaterial !== overrideMaterial ? overrideMaterial : null;
+  boxes.addInstances(1, (obj, index) => {
+    obj.position.x = index - 5;
+    obj.position.y = -1.5;
+  });
+}, 1000);
