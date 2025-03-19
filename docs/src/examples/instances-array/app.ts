6152c985b2dca9157dce75be5f395cec96af2366
Andrea Gargaro <devgargaro@gmail.com>
2025-03-19 13:20:47 +0100

    Documentation add basics page (#103)

--- docs/src/examples/instances-array/app.ts
+++ docs/src/examples/instances-array/app.ts
@@ -3,11 +3,18 @@ import { MeshStandardMaterial, TorusKnotGeometry } from 'three';
 
 const geo = new TorusKnotGeometry();
 const mat = new MeshStandardMaterial()
-export const torusKnots = new InstancedMesh2(geo, mat);
+export const torusKnots = new InstancedMesh2(geo, mat, { createEntities: true });
 
 torusKnots.addInstances(9, (obj, index) => {
   obj.position.x = (index % 3 - 1) * 5;
   obj.position.y = (Math.trunc(index / 3) - 1) * 5;
   obj.quaternion.random();
   obj.color = Math.random() * 0xffffff;
+});
+
+torusKnots.on('animate', (e) => {
+  for (const instance of torusKnots.instances) {
+    instance.rotateZ(e.delta);
+    instance.updateMatrix();
+  }
 });
