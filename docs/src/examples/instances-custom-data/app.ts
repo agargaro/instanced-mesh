baa08ef65b24e0df747d78888effa49b489d8a85
ANDREA GARGARO <devgargaro@gmail.com>
2025-03-19 22:35:35 +0100

    Update doc

---
+++ docs/src/examples/instances-custom-data/app.ts
@@ -0,0 +1,26 @@
+import { InstancedMesh2 } from '@three.ez/instanced-mesh';
+import { MeshStandardMaterial, TorusKnotGeometry } from 'three';
+
+type CustomData = { startScale: number };
+
+const geo = new TorusKnotGeometry();
+const mat = new MeshStandardMaterial()
+export const torusKnots = new InstancedMesh2<CustomData>(geo, mat, { createEntities: true });
+
+torusKnots.addInstances(9, (obj, index) => {
+  obj.position.x = (index % 3 - 1) * 5;
+  obj.position.y = (Math.trunc(index / 3) - 1) * 5;
+  obj.quaternion.random();
+  obj.color = Math.random() * 0xffffff;
+
+  obj.startScale = Math.random();
+  obj.scale.setScalar(obj.startScale);
+});
+
+torusKnots.on('animate', (e) => {
+  for (const instance of torusKnots.instances) {
+    instance.rotateZ(e.delta);
+    instance.scale.setScalar(Math.abs(Math.sin(instance.startScale + e.total)));
+    instance.updateMatrix();
+  }
+});
