07cd08da7e1730fa1104d3e9101eb69e3a58cec0
Luigi Denora <34819271+luigidenora@users.noreply.github.com>
2025-03-19 01:01:35 +0100

    docs: add getting-started section to the InstancedMesh documentation (#102)
    * first doc
    
    * fix: getting-started order and add pkg tabs
    
    * fix: remove banner from installation documentation
    
    ---------
    
    Co-authored-by: ANDREA GARGARO <devgargaro@gmail.com>

---
+++ docs/src/examples/first-scene/index.ts
@@ -0,0 +1,19 @@
+import { Scene, DirectionalLight, AmbientLight } from 'three';
+import { Main, PerspectiveCameraAuto } from '@three.ez/main';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { boxes } from './app.js';
+
+const main = new Main();
+const camera = new PerspectiveCameraAuto().translateZ(20);
+const scene = new Scene().add(boxes);
+main.createView({ scene, camera });
+
+const controls = new OrbitControls(camera, main.renderer.domElement);
+controls.update();
+
+const ambientLight = new AmbientLight('white', 0.8);
+scene.add(ambientLight);
+
+const dirLight = new DirectionalLight('white', 2);
+dirLight.position.set(0.5, 0.866, 0);
+camera.add(dirLight);
