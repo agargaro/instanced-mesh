e4c32b641665e9d6eaa68f61b21c8f57d398d4f6
Luigi Denora <denoraluigi24@gmail.com>
2025-03-22 17:51:54 +0100

    feat: enhance SpaceShip component with camera controls and lighting adjustments

--- docs/src/components/Intro/intro.ts
+++ docs/src/components/Intro/intro.ts
@@ -1,15 +1,27 @@
-import { InstancedMesh2 } from "@three.ez/instanced-mesh";
 import { Asset, Main, PerspectiveCameraAuto } from "@three.ez/main";
-import { AmbientLight, Scene } from "three";
+import { AmbientLight, OrthographicCamera, Scene } from "three";
 import { SpaceShip } from "./spaceship";
-import { Floor } from "./floor";
+import { Pane } from "tweakpane";
 
 await Asset.preloadAllPending();
 
-const main = new Main({ rendererParameters: { canvas: document.getElementById("three-canvas") } });
+window.pane = new Pane();
+pane.element.style.transform = "translateY(60px)";
+
+const cameraFolder = pane.addFolder({
+  title: "camera",
+  expanded: true,
+});
+
+const main = new Main({
+  rendererParameters: { canvas: document.getElementById("three-canvas") },
+});
 const scene = new Scene();
-const camera = new PerspectiveCameraAuto(50).rotateX(Math.PI / 4).translateZ(10).translateY(2);
+const camera = new PerspectiveCameraAuto().translateZ(10);
+cameraFolder.addBinding(camera, "fov", { min: 1, max: 179 });
+cameraFolder.addBinding(camera, "position", { x: {}, y: {}, z: {} });
+cameraFolder.addBinding(camera, "rotation", { x: {}, y: {}, z: {} });
 
 main.createView({ scene, camera });
 
-scene.add(new SpaceShip(), new Floor());
+scene.add(new SpaceShip(), new AmbientLight(0xffffff, 0.5));
