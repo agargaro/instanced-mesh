1603f2fff1c5494d56ee2d7e883dda37b8789bde
ANDREA GARGARO <devgargaro@gmail.com>
2025-03-23 19:09:15 +0100

    spaceship

--- docs/src/components/Intro/spaceship.ts
+++ docs/src/components/Intro/spaceship.ts
@@ -1,54 +1,49 @@
 import { Asset } from "@three.ez/main";
-import { BoxGeometry, Group, Mesh, MeshLambertMaterial, Vector3 } from "three";
-import { type GLTF, GLTFLoader } from "three/examples/jsm/Addons.js";
-import type { Pane } from "tweakpane";
-import { Smoke } from "./smoke";
+import { BufferGeometry, Group, Mesh, MeshLambertMaterial, PointLight, Vector2, Vector3 } from "three";
+import { GLTFLoader, type GLTF } from "three/examples/jsm/Addons.js";
 
 const GLB_PATH = "./instanced-mesh/low_poly_space_ship.glb";
-
 Asset.preload(GLTFLoader, GLB_PATH);
 
 export class SpaceShip extends Group {
   constructor() {
     super();
+    this.loadModel();
 
-    this.loadGltfAsset();
+    const pointLight = new PointLight('white', 20).translateY(5);
+    this.add(pointLight);
 
-    this.rotateY(Math.PI);
-    this.updateMatrix();
+    this.rotation.y = Math.PI;
+    this.translateY(5);
+    this.scale.setScalar(0.4);
 
-    this.setupPane();
+    this.bindInteraction();
   }
 
-  private loadGltfAsset() {
+  private loadModel(): void {
     const gltf = Asset.get<GLTF>(GLB_PATH);
-    console.assert(gltf, "[spaceship]: GLTF not loaded");
-
-    const group = gltf.scene.children[0];
-    console.assert(group, "[spaceship]: Group not found");
-
-    this.add(group);
-
-    const mesh = this.querySelector("Mesh");
-    mesh.material = new MeshLambertMaterial({
-      map: mesh.material.map,
-    });
+    const mesh = gltf.scene.querySelector("Mesh") as Mesh<BufferGeometry, MeshLambertMaterial>;
+    mesh.material = new MeshLambertMaterial({ map: mesh.material.map });
+    this.add(gltf.scene.children[0]);
   }
 
-  setupPane() {
-    const pane: Pane = window.pane;
-    if (pane) {
-      const spaceshipFolder = window.pane.addFolder({
-        title: this.constructor.name,
-        expanded: true,
-      });
+  private bindInteraction(): void {
+    const pointer = new Vector2();
+    const newPosition = new Vector3();
 
-      spaceshipFolder.addBinding(this, "position", {
-        label: this.constructor.name + " pos",
-      });
-      spaceshipFolder.addBinding(this, "rotation", {
-        label: this.constructor.name + " rotation",
-      });
-    }
+    window.addEventListener("mousemove", (e) => {
+      pointer.x = e.clientX / window.innerWidth - 0.5;
+      pointer.y = -e.clientY / window.innerHeight + 0.5;
+
+      newPosition.x = pointer.x * 10;
+      newPosition.z = pointer.y * 10;
+    });
+
+
+    this.on("animate", (e) => {
+      this.position.x += (newPosition.x - this.position.x) * e.delta * 10;
+      // this.position.z += (pointer.y - this.position.y) * e.delta;
+      this.rotation.z = (newPosition.x - this.position.x) * e.delta * 10;
+    });
   }
 }
