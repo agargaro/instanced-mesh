6412fb2e130b75b243f4fd0f48b042a13975e1da
Andrea Gargaro <devgargaro@gmail.com>
2025-04-09 22:52:04 +0200

    Fix raycasting and removed auto sorting (#115)
    * fix raycasting and removed auto sorting
    
    * Fix import unused

--- src/utils/SortingUtils.ts
+++ src/utils/SortingUtils.ts
@@ -1,7 +1,7 @@
-import { Intersection, Material } from 'three';
-import { InstancedRenderItem } from '../core/utils/InstancedRenderList.js';
+import { Material } from 'three';
 import { radixSort, RadixSortOptions } from 'three/addons/utils/SortUtils.js';
 import { InstancedMesh2 } from '../core/InstancedMesh2.js';
+import { InstancedRenderItem } from '../core/utils/InstancedRenderList.js';
 
 /**
  * Creates a radix sort function specifically for sorting `InstancedMesh2` instances.
@@ -52,9 +52,4 @@ export function sortOpaque(a: InstancedRenderItem, b: InstancedRenderItem): numb
 /** @internal */
 export function sortTransparent(a: InstancedRenderItem, b: InstancedRenderItem): number {
   return b.depth - a.depth;
-}
-
-/** @internal */
-export function ascSortIntersection(a: Intersection, b: Intersection): number {
-  return a.distance - b.distance;
 }
