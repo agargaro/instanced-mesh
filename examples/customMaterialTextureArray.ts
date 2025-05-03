7752a53b31bbb7e4cce8cec286568962b13a4616
GabrieleCoco28 <gabrielecoco0@gmail.com>
2025-05-03 14:04:23 +0200

    fixed ESLint warnings

--- examples/customMaterialTextureArray.ts
+++ examples/customMaterialTextureArray.ts
@@ -7,7 +7,7 @@ import { OrbitControls } from 'three/examples/jsm/Addons.js';
 export class TextureArrayMaterial extends ShaderMaterial {
   public dataTextureArray: DataArrayTexture;
 
-  public override vertexShader = /*glsl*/`
+  public override vertexShader = /* glsl */`
     #include <instanced_pars_vertex>
     #include <instanced_color_pars_vertex>
     varying vec2 vUv;
@@ -19,7 +19,7 @@ export class TextureArrayMaterial extends ShaderMaterial {
         gl_Position = projectionMatrix * modelViewMatrix * instanceMatrix * vec4(position, 1.0);
     }`;
 
-  public override fragmentShader = /*glsl*/`
+  public override fragmentShader = /* glsl */`
     uniform float textureIndex;
     uniform sampler2DArray textureArray;
     varying vec2 vUv;
@@ -39,10 +39,10 @@ export class TextureArrayMaterial extends ShaderMaterial {
 
   protected createDataArrayTexture(textures: Texture[], width: number, height: number): DataArrayTexture {
     const textureCount = textures.length;
-    const canvas = document.createElement("canvas");
+    const canvas = document.createElement('canvas');
     canvas.width = width;
     canvas.height = height;
-    const ctx = canvas.getContext("2d", { willReadFrequently: true });
+    const ctx = canvas.getContext('2d', { willReadFrequently: true });
     const pixels = new Uint8Array(width * height * 4 * textureCount);
 
     for (let i = 0; i < textureCount; i++) {
@@ -65,7 +65,7 @@ export class TextureArrayMaterial extends ShaderMaterial {
 const textures = [
   await Asset.load<Texture>(TextureLoader, 'planks.jpg'),
   await Asset.load<Texture>(TextureLoader, 'wall.jpg'),
-  await Asset.load<Texture>(TextureLoader, 'pattern.jpg'),
+  await Asset.load<Texture>(TextureLoader, 'pattern.jpg')
 ];
 
 const instancedMesh = new InstancedMesh2(new SphereGeometry(0.25, 16, 16), new TextureArrayMaterial(textures));
@@ -74,11 +74,11 @@ instancedMesh.initUniformsPerInstance({ fragment: { textureIndex: 'float' } });
 instancedMesh.addInstances(1000, (obj, index) => {
   obj.position.copy(obj.position.random().subScalar(0.5).multiplyScalar(20));
   obj.setUniform('textureIndex', Math.round(Math.random() * (textures.length - 1)));
-})
+});
 
 const scene = new Scene().add(instancedMesh);
 const main = new Main();
 const camera = new PerspectiveCameraAuto(70).translateZ(10);
 const controls = new OrbitControls(camera, main.renderer.domElement);
-controls.update()
+controls.update();
 main.createView({ scene, camera });
