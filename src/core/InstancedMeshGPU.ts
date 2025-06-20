dae604a6c24c9a6f05d0a3127a8cae6876c7249d
Ian <ionox0@gmail.com>
2025-06-20 11:28:18 -0400

    fix typing

--- src/core/InstancedMeshGPU.ts
+++ src/core/InstancedMeshGPU.ts
@@ -2,41 +2,25 @@ import { BufferGeometry, ColorManagement, DataTexture, Material, Object3DEventMa
 import { SquareDataTextureGPU } from './utils/SquareDataTexture.js';
 import { MeshBasicNodeMaterial, StorageInstancedBufferAttribute, WebGPURenderer } from 'three/webgpu';
 import { getColorTexture, getInstancedMatrix } from '../shaders/tsl/nodes.js';
-import { InstancedMesh2 } from './InstancedMesh2.js';
+import { InstancedMesh2, InstancedMesh2Params } from './InstancedMesh2.js';
 
 
 /**
  * Parameters for configuring an `InstancedMeshGPU` instance.
  */
-export interface InstancedMeshGPUParams {
-  /**
-   * Determines the maximum number of instances that buffers can hold.
-   * The buffers will be expanded automatically if necessary.
-   * @default 1000
-   */
+export interface InstancedMeshGPUParams extends Omit<InstancedMesh2Params, 'renderer'> {
+  
   capacity?: number;
-  /**
-   * Determines whether to create an array of `InstancedEntity` to easily manipulate instances at the cost of more memory.
-   * @default false
-   */
+  
   createEntities?: boolean;
-  /**
-   * Determines whether `InstancedEntity` can use the `rotation` property.
-   * If `true` `quaternion` and `rotation` will be synchronized, affecting performance.
-   * @default false
-   */
+  
   allowsEuler?: boolean;
-  /**
-   * WebGL renderer instance.
-   * If not provided, buffers will be initialized during the first render, resulting in no instances being rendered initially.
-   * @default null
-   */
+  
   renderer?: WebGPURenderer;
 }
 
 /**
  * Alternative `InstancedMesh` class to support additional features like frustum culling, fast raycasting, LOD and more.
- * @template TData Type for additional instance data.
  * @template TGeometry Type extending `BufferGeometry`.
  * @template TMaterial Type extending `Material` or an array of `Material`.
  * @template TEventMap Type extending `Object3DEventMap`.
@@ -46,78 +30,44 @@ export class InstancedMeshGPU<
   TGeometry extends BufferGeometry = BufferGeometry,
   TMaterial extends Material | Material[] = Material | Material[],
   TEventMap extends Object3DEventMap = Object3DEventMap
-> extends InstancedMesh2<TGeometry, TMaterial, TEventMap> {
-  /**
-   * @defaultValue `InstancedMesh2`
-   */
+> extends InstancedMesh2<TData, TGeometry, TMaterial, TEventMap> {
+  
   public override readonly type = 'InstancedMeshGPU' as any;
-  /**
-   * Indicates if this is an `InstancedMeshGPU`.
-   */
+  
   public readonly isInstancedMeshGPU = true;
-  /**
-   * Texture storing matrices for instances.
-   */
+  
   public override matricesTexture: SquareDataTextureGPU;
-  /**
-   * Texture storing colors for instances.
-   */
+  
   public override colorsTexture: SquareDataTextureGPU = null;
-  /**
-   * Texture storing bones for instances.
-   */
+  
   public override boneTexture: SquareDataTextureGPU = null;
-  /**
-   * Texture storing custom uniforms per instance.
-   */
+  
   public override uniformsTexture: SquareDataTextureGPU = null;
 
-  /** @internal */ override _renderer: WebGPURenderer | any = null;
+  override _renderer: WebGPURenderer | any = null;
 
-  /**
-   * Material for TSL nodes system
-   */
-  protected override _currentMaterial: MeshBasicNodeMaterial = null;
+  override _currentMaterial: MeshBasicNodeMaterial = null;
 
-  /** @internal */ override instanceMatrix = new StorageInstancedBufferAttribute(new Float32Array(0), 16);
+  override instanceMatrix = new StorageInstancedBufferAttribute(new Float32Array(0), 16);
 
   private _adapter: GPUAdapter;
   private _device: GPUDevice;
   private _context: any;
 
-  /** @internal */
-  // eslint-disable-next-line @typescript-eslint/unified-signatures
-  constructor(geometry: TGeometry, material: TMaterial, params?: InstancedMeshGPUParams, LOD?: InstancedMeshGPU);
-  constructor(geometry: TGeometry, material: TMaterial, params?: InstancedMeshGPUParams);
-  /**
-   * @remarks Geometry cannot be shared. If reused, it will be cloned.
-   * @param geometry An instance of `BufferGeometry`.
-   * @param material A single or an array of `Material`.
-   * @param params Optional configuration parameters object. See `InstancedMeshGPUParams` for details.
-   */
   constructor(geometry: TGeometry, material: TMaterial, params: InstancedMeshGPUParams = {}, LOD?: InstancedMeshGPU) {
     if (!geometry) throw new Error('"geometry" is mandatory.');
     if (!material) throw new Error('"material" is mandatory.');
 
-    const { allowsEuler, renderer, createEntities } = params;
-
-    super(geometry, material);
-
-    const capacity = params.capacity > 0 ? params.capacity : _defaultCapacity;
-    this._renderer = renderer;
-    this._capacity = capacity;
-    this._createEntities = createEntities;
-    this.initMatricesTexture();
+    super(geometry, material, params, LOD);
+    this.init();
   }
 
-  /**
-   * Initializes the `InstancedMeshGPU` instance.
-   * This method is called automatically when the instance is created.
-   */
   public async init(): Promise<void> {
     this._adapter = await navigator.gpu.requestAdapter();
     this._device = await this._adapter.requestDevice();
     this._context = this._renderer.domElement.getContext('webgpu');
+    this.initMatricesTexture();
+    this.initColorsTexture();
   }
 
   protected override initMatricesTexture(): void {
