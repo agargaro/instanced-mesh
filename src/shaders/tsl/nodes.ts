a05eeb38b92fa71aee5f155cf9ac5400df2bfbba
Ian <ionox0@gmail.com>
2025-06-20 16:48:26 -0400

    change deprecated fn

--- src/shaders/tsl/nodes.ts
+++ src/shaders/tsl/nodes.ts
@@ -4,7 +4,7 @@ import { Fn, instanceIndex, int, ivec2, mat4, textureSize, uniform, vec4 } from 
 export const getColorTexture = (colorsTexture) => {
     const size = int(textureSize(colorsTexture, int(0)).x).toVar();
     const j = int(instanceIndex).toVar();
-    const x = int(j.remainder(size)).toVar();
+    const x = int(j.mod(size)).toVar();
     const y = int( j.div( size ) ).toVar();
     return colorsTexture.sample( ivec2( x, y ) ).setSampler( false );
 };
@@ -13,7 +13,7 @@ export const getColorTexture = (colorsTexture) => {
 export const getInstancedMatrix = (matricesTexture) => {
 	const size = int( textureSize( matricesTexture, int( 0 ) ).x ).toVar();
 	const j = int( int( instanceIndex ).mul( int( 4 ) ) ).toVar();
-	const x = int( j.remainder( size ) ).toVar();
+	const x = int( j.mod( size ) ).toVar();
 	const y = int( j.div( size ) ).toVar();
 	const v1 = vec4( matricesTexture.sample( ivec2( x, y ) ).setSampler( false ) ).toVar();
 	const v2 = vec4( matricesTexture.sample( ivec2( x.add( int( 1 ) ), y ) ).setSampler( false ) ).toVar();
@@ -28,7 +28,7 @@ export const getBoneMatrix = (boneTexture) => Fn((i) => {
 
 	const size = int( textureSize( boneTexture, int( 0 ) ).x ).toVar();
 	const j = int( bonesPerInstance.mul( int( instanceIndex ) ).add( int( i ) ).mul( int( 4 ) ) ).toVar();
-	const x = int( j.remainder( size ) ).toVar();
+	const x = int( j.mod( size ) ).toVar();
 	const y = int( j.div( size ) ).toVar();
 	const v1 = vec4( boneTexture.sample( ivec2( x, y ) ).setSampler( false ) ).toVar();
 	const v2 = vec4( boneTexture.sample( ivec2( x.add( int( 1 ) ), y ) ).setSampler( false ) ).toVar();
